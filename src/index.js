d66533aaecf8b32cf0b3fedd63c1b71b79b3310d
LAPTOP-TQPOKIGM\HP <fauzanahmad0509@gmail.com>
2026-05-01 20:14:54 +0700

    fixing configuration

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const laporanRoutes = require('./routes/laporanRoutes')
 
 // Middleware global
 app.use(helmet());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:8081'], credentials: true }))
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
